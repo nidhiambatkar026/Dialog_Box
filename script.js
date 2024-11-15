b2e6fb24e98f68cc1882d4bcafcedbbadc48ad2e
nidhiambatkar026 <nidhiambatkar@gmail.com>
2024-11-15 12:23:28 +0530

    javascript added

---
+++ script.js
@@ -0,0 +1,8 @@
+function openoverlay(){
+    const OverlayElement = document.getElementById('overlay');
+    OverlayElement.style.display = 'flex';
+}
+function closeoverlay(){
+    const OverlayElement = document.getElementById('overlay');
+    OverlayElement.style.display = 'none';
+}
